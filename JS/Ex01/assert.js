59b14131055b70feb0602fbd72234ac26c85ef44
Aviad Hahami <aviadhahami@gmail.com>
2016-04-04 01:01:48 +0300

    Working on tests

--- JS/Ex01/assert.js
+++ JS/Ex01/assert.js
@@ -1,20 +1,26 @@
 /**
  * Created by aviad on 4/4/2016.
  */
+function init(){
+
+    document.body.innerHTML += '<h1>Tests results</h1>';
+};
+init();
 
 window.assert = {
     Equals: function (arg1, arg2, testName) {
         if (arg1 === arg2) {
-            console.log('%c Test Passed for '+ testName, 'background: green; color: white');
+            document.body.innerHTML += '<p style="background: green; color: white;">Test Passed for ' + testName+'</p>';
         } else {
-            console.log('%c Test Failed for '+ testName, 'background: red; color: white');
+            document.body.innerHTML += '<p style="background: darkred; color: white;">Test Failed for ' + testName+'</p>';
         }
     },
     BiggerThan: function (arg1, arg2, testName) {
         if (arg1 > arg2) {
-            console.log('%c Test passed for '+ testName, 'background: green; color: white');
+            document.body.innerHTML += '<p style="background: green; color: white;">Test Passed for ' + testName + '</p>';
         } else {
-            console.log('%c Test Failed for '+ testName, 'background: red; color: white');
+            document.body.innerHTML += '<p style="background: darkred; color: white;">Test Failed for ' + testName + '</p>';
+
         }
     }
 };
