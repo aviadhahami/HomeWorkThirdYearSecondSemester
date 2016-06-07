fd9a059865dfbbe37212fd8c91c5d01e01a1815a
Aviad Hahami <aviadhahami@gmail.com>
2016-06-08 00:15:25 +0300

    Added quotes

--- JS/Ex03/Aviad/server/stores/server.stores.quotesStore.js
+++ JS/Ex03/Aviad/server/stores/server.stores.quotesStore.js
@@ -1,5 +1,9 @@
 /**
  * Created by aviad on 6/8/2016.
  */
-
-module.exports = [];
+'use strict';
+module.exports = [
+	'A person is smart. People are dumb, panicky dangerous animals and you know it. Fifteen hundred years ago everybody knew the Earth was the center of the universe. Five hundred years ago, everybody knew the Earth was flat, and fifteen minutes ago, you knew that humans were alone on this planet. Imagine what you\'ll know tomorrow.',
+	'I love bad bitches that\'s my fucking problem',
+	'Only dead fish flow with the stream'
+];
