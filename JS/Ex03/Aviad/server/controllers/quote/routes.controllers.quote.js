78f432dedd597072b484686df18af52ca57362ab
Aviad Hahami <aviadhahami@gmail.com>
2016-06-08 00:24:32 +0300

    fixed range bug

--- JS/Ex03/Aviad/server/controllers/quote/routes.controllers.quote.js
+++ JS/Ex03/Aviad/server/controllers/quote/routes.controllers.quote.js
@@ -5,6 +5,6 @@
 const quotesStore = require('../../stores/server.stores.quotesStore'),
 	randomizer = require('../../external_modules/server.external_modules.randomGenerator');
 let QuotesService = {
-	retrieveRandomQuote: (req,res)=> res.status(200).json({quote:quotesStore[randomizer.random(0,quotesStore.length-1)]})
+	retrieveRandomQuote: (req,res)=> res.status(200).json({quote:quotesStore[randomizer.random(0,quotesStore.length)]})
 };
 module.exports = QuotesService;
