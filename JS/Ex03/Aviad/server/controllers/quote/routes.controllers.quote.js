24d402845f8dcf7f3da2ac7e2c1f19f8aac41128
Aviad Hahami <aviadhahami@gmail.com>
2016-06-08 00:15:36 +0300

    randomizer and quotes

--- JS/Ex03/Aviad/server/controllers/quote/routes.controllers.quote.js
+++ JS/Ex03/Aviad/server/controllers/quote/routes.controllers.quote.js
@@ -1,6 +1,7 @@
 /**
  * Created by aviad on 6/8/2016.
  */
+'use strict';
 const quotesStore = require('../../stores/server.stores.quotesStore'),
 	randomizer = require('../../external_modules/server.external_modules.randomGenerator');
 let QuotesService = {
--- JS/Ex03/Aviad/server/external_modules/server.external_modules.randomGenerator.js
+++ JS/Ex03/Aviad/server/external_modules/server.external_modules.randomGenerator.js
@@ -1,7 +1,7 @@
 /**
  * Created by aviad on 6/8/2016.
  */
-
+'user strict';
 module.exports = {
 
 	// Randomizes a number between s and e
