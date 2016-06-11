d1e236977b925913df5228a231bd789e59f69443
Aviad Hahami <aviadhahami@gmail.com>
2016-06-12 02:11:34 +0300

    some css and quotePanel design

--- JS/Ex03/Aviad/app/components/profile/profile.js
+++ JS/Ex03/Aviad/app/components/profile/profile.js
@@ -3,41 +3,45 @@
  */
 import React from 'react';
 import {Jumbotron, Button, Image, Media, Col, Row} from 'react-bootstrap';
+import QuotePanel from '../quotePanel/quotePanel'
 const profilePic = require('../../assets/img/profile.jpg');
 
 let Profile = React.createClass({
 	render(){
 		return(
-			<Jumbotron>
-				<Media>
-					<Media.Left align="middle">
-						<Image width={'auto'} height={300} src={profilePic} rounded/>
-					</Media.Left>
-					<Media.Body>
-						<h1>Aviad Hahami</h1>
-						<p>
-							After two years with AngularJS I've decided I need something fresh.
+			<div>
+				<Jumbotron>
+					<Media>
+						<Media.Left align="middle">
+							<Image width={'auto'} height={300} src={profilePic} rounded/>
+						</Media.Left>
+						<Media.Body>
+							<h1>Aviad Hahami</h1>
+							<p>
+								After two years with AngularJS I've decided I need something fresh.
+								<br/>
+								As a result - I've started learning React.
+								<br/>
+								Hope you enjoy this :)
+							</p>
 							<br/>
-							As a result - I've started learning React.
-							<br/>
-							Hope you enjoy this :)
-						</p>
-						<br/>
-						<Row>
-							<Col xs={6} md={4} >
-								<Button bsStyle="primary">
-									<a href="https://github.com/aviadhahami" target="_blank">See my GitHub</a>
-								</Button>
-							</Col>
-							<Col xs={6} md={4} >
-								<Button bsStyle="success">
-									<a href="https://www.linkedin.com/in/aviad-hahami-09058133" target="_blank">See my linkedin</a>
-								</Button>
-							</Col>
-						</Row>
-					</Media.Body>
-				</Media>
-			</Jumbotron>
+							<Row>
+								<Col xs={6} md={4} >
+									<Button bsStyle="primary">
+										<a href="https://github.com/aviadhahami" target="_blank">See my GitHub</a>
+									</Button>
+								</Col>
+								<Col xs={6} md={4} >
+									<Button bsStyle="success">
+										<a href="https://www.linkedin.com/in/aviad-hahami-09058133" target="_blank">See my linkedin</a>
+									</Button>
+								</Col>
+							</Row>
+						</Media.Body>
+					</Media>
+				</Jumbotron>
+				<QuotePanel/>
+			</div>
 		)
 	}
 });
---
+++ JS/Ex03/Aviad/app/components/quotePanel/quotePanel.js
@@ -0,0 +1,32 @@
+/**
+ * Created by aviad on 6/12/2016.
+ */
+import React from 'react'
+import {connect} from 'cartiv';
+import Api from '../../stores/Api'
+import quoteStore from '../../stores/quotesStore'
+let quotePanel = React.createClass({
+	mixins:[
+		connect(quoteStore,'quote')
+	],
+	componentWillMount(){
+		Api.quotes.onGetQuote();
+	},
+	render(){
+		let {quote} = this.state
+		return(
+			<div className="container">
+				<blockquote className="quote-box">
+					<p className="quotation-mark">
+						“
+					</p>
+					<p className="quote-text">
+						{quote || 'Loading...'}
+					</p>
+				</blockquote>
+			</div>
+		)
+	}
+});
+
+export default quotePanel;
